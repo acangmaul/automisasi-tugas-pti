0b83511b8cc94a3ceb09385a8d3b4368049bebeb
ibldzn <51160226+ibldzn@users.noreply.github.com>
2022-12-17 08:11:57 +0700

    fix: update key and sum other stuff

--- main.py
+++ main.py
@@ -1,31 +1,37 @@
-from datetime import datetime, date
+from datetime import datetime, date, tzinfo
+from gspread.auth import Path
 from gspread.spreadsheet import ExportFormat
 import gspread
 import sys
 import locale
+from typing import List, Any
 
-locale.setlocale(locale.LC_ALL,
-                 "id-ID" if sys.platform == "win32" else "id_ID.UTF-8")
+
+locale.setlocale(locale.LC_ALL, "id-ID" if sys.platform == "win32" else "id_ID.UTF-8")
+
+
+def flatten(l: List[List[Any]]) -> List:
+    return [item for sublist in l for item in sublist]
 
 
 def main():
-    service_account = gspread.service_account(filename="creds.json")
-    doc = service_account.open_by_key("1CKSNp_cAqZ7wIO-SDjOqh7FqrBSEF6L2VbE3Dz19X7E")
+    service_account = gspread.service_account(filename=Path("creds.json"))
+    doc = service_account.open_by_key("1glDtHOViEcNfRVGuItSiuYv7iguWBaOQUFKXpI46Zug")
 
-    with open(f"Laporan {date.today().strftime('%d-%b-%Y')}.pdf", "wb") as f:
+    sheet = doc.sheet1
+
+    range_tanggal = "A3:F3"
+    tanggal = flatten(sheet.get(range_tanggal))
+
+    assert len(tanggal) == 1, "Format tanggal tidak cocok"
+
+    tanggal = tanggal[0]
+
+    with open(f"LAPORAN {tanggal}.pdf", "wb") as f:
         pdf = doc.export(ExportFormat.PDF)
         f.write(pdf)
 
-    # kalau jam 12 malam
-    # update tanggal dan kosongkan spreadsheet
-    if datetime.now().hour == 0:
-        sheet = doc.sheet1
-
-        range_tanggal = "A3:F3"
-        range_nama_npm = "B5:C44"
-
-        sheet.update(range_tanggal, date.today().strftime("%A, %d %B %Y").upper())
-        sheet.batch_clear(range_nama_npm)
+    sheet.update(range_tanggal, date.today().strftime("%A, %d %B %Y").upper())
 
 
 if __name__ == "__main__":
